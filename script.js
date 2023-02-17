ca8955497a1b6190a6f43eb3812e4b9880313c6c
c0dag <114384142+c0dag@users.noreply.github.com>
2023-02-17 13:57:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const audioPlayer = document.getElementById('audio-player');
 const playPauseBtn = document.querySelector('.play-pause-btn');
+const nightModeBtn = document.querySelector('.night-mode-btn');
+const modelViewer = document.querySelector('model-viewer');
 
 playPauseBtn.addEventListener('click', function() {
   if (audioPlayer.paused) {
@@ -10,9 +12,6 @@ playPauseBtn.addEventListener('click', function() {
     playPauseBtn.innerHTML = '<i class="fas fa-play"></i>';
   }
 });
-
-const nightModeBtn = document.querySelector('.night-mode-btn');
-const modelViewer = document.querySelector('model-viewer');
 
 let isNightMode = false;
 
@@ -28,9 +27,20 @@ nightModeBtn.addEventListener('click', function() {
   }
 });
 
-// detect if the user has set their computer to prefer dark mode
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   nightModeBtn.innerHTML = '<i class="fas fa-sun"></i>';
   modelViewer.style.backgroundColor = 'rgb(10, 10, 10)';
   isNightMode = true;
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Space') {
+        if (audioPlayer.paused) {
+            audioPlayer.play();
+            playPauseBtn.innerHTML = '<i class="fas fa-pause"></i>';
+        } else {
+            audioPlayer.pause();
+            playPauseBtn.innerHTML = '<i class="fas fa-play"></i>';
+        }
+    }
+});
